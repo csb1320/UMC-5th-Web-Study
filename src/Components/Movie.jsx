77f1e5f162eb0b9ff6e6c60eb5ce1d9380f8a78c
Seongbin <122718910+csb1320@users.noreply.github.com>
2023-10-26 10:56:52 +0900

    Update Movie.jsx

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import { MovieContainer, MovieInfo, Overview } from './Movie.style';
 
 const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
 function Movie(props) {
     return (
-        <div className="movie-container">
+        <MovieContainer>
             <img src={`${IMG_BASE_URL}${props.movieData.poster_path}`} alt="영화 포스터" />
-            <div className='movie-info'>
+            <MovieInfo>
                 <h4>{props.movieData.title}</h4>
                 <span>{props.movieData.vote_average}</span>
-            </div>
+            </MovieInfo>
 
-            <div className="overview">
+            <Overview>
                 <h4>{props.movieData.title}</h4>
                 <p>{props.movieData.overview}</p>
-            </div>
-        </div>
+            </Overview>
+        </MovieContainer>
     );
   }
   

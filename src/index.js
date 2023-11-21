1c39be6be426a5a41dc9139363f674f4520b41e3
seongbin <csb1320@naver.com>
2023-11-21 15:36:44 +0900

    openAPI

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import WeatherApp from './Components/WeatherApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WeatherApp />
   </React.StrictMode>
 );
 
